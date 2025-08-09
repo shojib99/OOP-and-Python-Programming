512d235f716e39ef1e54ef9604135aece472e0ca
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 19:11:38 +0600

    Cheaking Membership of SARC.py

---
+++ Cheaking Membership of SARC.py
@@ -0,0 +1,5 @@
+sarc= ['Bangladesh', 'Afganistan','Nepal','Bhutan','lanka']
+print (sarc)
+print ("Bangladesh in sarc" ,"Bangladesh" in sarc)
+print ("China in sarc", "China" in sarc)
+print ("Nepal in sarc", "Nepal" in sarc)
