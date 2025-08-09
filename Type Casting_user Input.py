2ee8eec334a0a20209cc45b6adba48d6cba01884
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 19:01:15 +0600

    Update Type Casting_user Input.py

--- Type Casting_user Input.py
+++ Type Casting_user Input.py
@@ -1,2 +1,7 @@
 s=input("Enter any value: ")
 print(type(s))
+
+number1= input ("please type an integer and press center: ")
+number2 = input ("please type another integer and press center: ")
+ 
+print ("number1+number2 = ", number1 + number2)
