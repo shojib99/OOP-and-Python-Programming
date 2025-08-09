28da755f5eb74e9d45e87c98ed25ace17dda7b75
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 19:12:28 +0600

    If Statement.py

---
+++ If Statement.py
@@ -0,0 +1,5 @@
+sarc = ['Bangladesh', 'Afganistan', 'Nepal', 'Bhutan', 'lanka']
+country = input("Enter the name of the country: ")
+if country in sarc:
+    print(country, "is a member in sarc")
+    print("program terminated")
