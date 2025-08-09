7e832bd88e778eb977af060dc0f28a6e5cca80b6
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 19:02:44 +0600

    Type Casting_Solve.py

---
+++ Type Casting_Solve.py
@@ -0,0 +1,5 @@
+number1= int(input ("please type an integer and press center: "))
+number2 = int(input ("please type another integer and press center: "))
+ 
+print ("number1+number2 = ", number1 + number2)
+ 
