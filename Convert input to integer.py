ac4afc2c2b622cd3410f41c840f33be3b1082e1d
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 19:04:03 +0600

    Convert input to integer.py

---
+++ Convert input to integer.py
@@ -0,0 +1,6 @@
+number1= int(input ("please type an integer and press center: "))
+number2 = int(input ("please type another integer and press center: "))
+number1 = int (number1)
+number2 = int (number2)
+ 
+print ("number1+number2 = ", number1 + number2)
