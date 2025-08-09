c57b791bf35924ef28b8a4cf5ec7dfaeac471177
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-08-09 18:16:01 +0600

    Math operation with user input.py

---
+++ Math operation with user input.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your Number1: "))
+b = int(input("Enter your Number2: "))
+
+add = a + b
+print("Addition is : " + str(add))
+
+sub = a - b
+print("Subtraction is : " + str(sub))
+
+mul = a * b
+print("Multiplication is : " + str(mul))
+
+div = a / b
+print("Division is : " + str(div))
+
+mod = a % b
+print("Modulus is : " + str(mod))
+
