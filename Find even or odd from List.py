b7c6d3274e894039afe46edb5da549199b1eee83
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2025-10-12 01:06:54 +0600

    Find even or odd from List.py

---
+++ Find even or odd from List.py
@@ -0,0 +1,13 @@
+numbers = [10, 23, 45, 66, 78, 91, 100]
+
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
