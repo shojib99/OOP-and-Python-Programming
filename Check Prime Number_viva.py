b92b5f1876f0299d06ef91c34aa20240b14d347d
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2026-01-05 00:23:06 +0600

    Add prime number check functionality

---
+++ Check Prime Number_viva.py
@@ -0,0 +1,15 @@
+num = 13
+
+if num <= 1:
+    print("Not Prime")
+else:
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime Number")
+    else:
+        print("Not Prime")
