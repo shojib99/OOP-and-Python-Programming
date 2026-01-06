05c24e3bff0d3971f39c4808a4ffbe1eb8b14f9a
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2026-01-07 00:38:49 +0600

    Add Armstrong number check functionality

---
+++ Check Armstrong Number_viva.py
@@ -0,0 +1,14 @@
+num = 153
+sum = 0
+temp = num
+n = len(str(num))
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** n
+    temp //= 10
+
+if num == sum:
+    print(f"{num} is an Armstrong number")
+else:
+    print(f"{num} is not an Armstrong number")
