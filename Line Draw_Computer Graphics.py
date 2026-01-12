c8bd28de94316f9e70b3ac49a8151eea9105c0ae
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2026-01-12 22:53:55 +0600

    Add line drawing functionality using matplotlib

---
+++ Line Draw_Computer Graphics.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y = [5,8,7,10]
+plt.plot(x,y,marker="X",color="red",linestyle="dashed",linewidth="5",markersize="20")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Line Draw")
+plt.show()
